ff4a4aa0dd0cbe4ee02ded93673823fda7cf0f63
Michal Dzidziewicz <dzidziewicz_michal@interia.pl>
2019-10-12 19:24:20 +0200

    added neutral network class

---
+++ src/neural_network.py
@@ -0,0 +1,12 @@
+from layer import Layer
+
+class NeuralNetwork:
+    def __init__(self):
+        self.layers = []
+    
+    def add_layer(self, layer):
+        self.layers.append(layer)
+
+    def create_and_add_layer(self, input_count, neuron_count, activation_function, weights = None, biases = None):
+        layer = Layer(input_count, neuron_count, activation_function, weights, biases)
+        self.add_layer(layer)
