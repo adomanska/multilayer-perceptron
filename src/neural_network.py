689a06a47ee19694e5cc610b4782f0cb75fd9f28
Michal Dzidziewicz <dzidziewicz_michal@interia.pl>
2019-10-12 21:32:49 +0200

    added feed forward method

--- src/neural_network.py
+++ src/neural_network.py
@@ -10,3 +10,9 @@ class NeuralNetwork:
     def create_and_add_layer(self, input_count, neuron_count, activation_function, weights = None, biases = None):
         layer = Layer(input_count, neuron_count, activation_function, weights, biases)
         self.add_layer(layer)
+
+    def feed_forward(self, input):
+        for layer in self.layers:
+            input = layer.activate(input)
+
+        return input
