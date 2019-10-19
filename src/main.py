Fix backpropagation

src/main.py
@@ -1,6 +1,6 @@
-from .neural_network.neural_network import NeuralNetwork
-from .activation_functions.sigmoid import Sigmoid
-from .activation_functions.re_lu import ReLU
+from neural_network.neural_network import NeuralNetwork
+from activation_functions.sigmoid import Sigmoid
+from activation_functions.re_lu import ReLU
 import numpy as np
 
 nn = NeuralNetwork()
@@ -9,10 +9,10 @@ nn.create_and_add_layer(2, 2, Sigmoid())
 nn.create_and_add_layer(2, 2, Sigmoid(), True)
 
 data = np.array([
-    ([0.1, 0.1], 0),
-    ([0.2, 0.2], 1),
-    ([0.3, 0.3], 0),
-    ([0.4, 0.4], 1)
+    ([0.1, 0.1], [1, 0]),
+    ([0.2, 0.2], [0, 1]),
+    ([0.3, 0.3], [1, 0]),
+    ([0.4, 0.4], [0, 1])
 ])
 
 test_data = np.array([
@@ -22,5 +22,5 @@ test_data = np.array([
     ([0.4, 0.4], 1)
 ])
 
-nn.train(data, 4, 1000, 0.001)
+nn.train(data, 4, 100, 3)
 print(nn.evaluate(test_data))

src/neural_network/neural_network.py
@@ -59,14 +59,15 @@ class NeuralNetwork:
         delta = None
         nabla_b = []
         nabla_w = []
-        for layer in reversed(self.layers):
-            delta, nw = layer.backward_pass(y, delta)
+        for i in range(1, len(self.layers) + 1):
+            layer = self.layers[-i]
+            delta, nw = layer.backward_pass(y, delta, self.layers[-i + 1].weights)
             nabla_b.append(delta)
             nabla_w.append(nw)
         return (nabla_b, nabla_w)
 
     def evaluate(self, test_data):
-        test_results = [(np.argmax(self._feed_forward(x)), y)
+        test_results = [(np.argmax(self._feed_forward(np.array(x))), y)
                         for (x, y) in test_data]
         print(test_results)
         return sum(int(x == y) for (x, y) in test_results)