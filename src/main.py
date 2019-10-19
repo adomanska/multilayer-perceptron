7235f897e4e9c8a6827843abdddc82e8afc664d8
Andżelika Domańska <andzelika.domanska@prudential.pl>
2019-10-19 21:00:48 +0200

    Use of test data

--- src/main.py
+++ src/main.py
@@ -5,33 +5,22 @@ import numpy as np
 from data_reader import DataReader
 
 nn = NeuralNetwork()
-nn.create_and_add_layer(2, 10, Sigmoid())
-nn.create_and_add_layer(10, 15, Sigmoid())
+nn.create_and_add_layer(2, 10, Sigmoid(), False)
+nn.create_and_add_layer(10, 15, Sigmoid(), False)
 nn.create_and_add_layer(15, 2, Sigmoid(), True)
 
-# data = np.array([
-#     ([0.1, 0.1], [1, 0]),
-#     ([0.2, 0.2], [0, 1]),
-#     ([0.3, 0.3], [1, 0]),
-#     ([0.4, 0.4], [0, 1])
-# ])
-
-# test_data = np.array([
-#     ([0.1, 0.1], 0),
-#     ([0.2, 0.2], 1),
-#     ([0.3, 0.3], 0),
-#     ([0.4, 0.4], 1)
-# ])
 def create_output(i, n):
     output = np.zeros(n)
     output[i - 1] = 1
     return output
 
-data_reader = DataReader("./data/classification/train/data.simple.train.100.csv")
-xs = data_reader.get_columns(["x", "y"])
-ys = data_reader.get_columns(["cls"])
-
+train_data_reader = DataReader("./data/classification/train/data.simple.train.1000.csv")
+xs = train_data_reader.get_columns(["x", "y"])
+ys = train_data_reader.get_columns(["cls"])
 train_data = [(x, create_output(int(y), 2)) for x, y in zip(xs, ys)]
+test_data_reader = DataReader("./data/classification/test/data.simple.test.1000.csv")
+xs = test_data_reader.get_columns(["x", "y"])
+ys = test_data_reader.get_columns(["cls"])
 test_data = [(x, y - 1) for x, y in zip(xs, ys)]
 
-nn.train(train_data, 10, 100, 1, test_data)
+nn.train(train_data, 10, 100, 0.5, test_data)
