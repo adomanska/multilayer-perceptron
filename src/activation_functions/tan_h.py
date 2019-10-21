44aa447fa98289832661dddb2d89b59e5b3ab407
adomanska <domanskaa@student.mini.pw.edu.pl>
2019-10-21 20:58:59 +0200

    Add nn implementations for specific problem type, add regression example

--- src/activation_functions/tan_h.py
+++ src/activation_functions/tan_h.py
@@ -1,9 +1,10 @@
 from .activation_function import ActivationFunction
 import numpy as np
+import math
 
 class TanH(ActivationFunction):
     def calculate(self, x):
         return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 
     def calculate_derivative(self, x):
-        return 1 - self.calculate(x) ^ 2
+        return 1 - np.power(self.calculate(x) , 2)
