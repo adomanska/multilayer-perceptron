13a62dbcbeef2fa46181ef19b0afedb363cee861
Andżelika Domańska <andzelika.domanska@prudential.pl>
2019-10-25 12:57:29 +0200

    Uncomment code

--- src/main.py
+++ src/main.py
@@ -9,15 +9,15 @@ from data_transformations import create_test_data, create_train_data, ProblemTyp
 import matplotlib.pyplot as plt
 
 # Classification
-# print("Classification")
-# nn = ClassificationNeuralNetwork()
-# nn.create_and_add_layer(2, 10, Sigmoid(), False)
-# nn.create_and_add_layer(10, 15, Sigmoid(), False)
-# nn.create_and_add_layer(15, 2, Sigmoid(), True)
+print("Classification")
+nn = ClassificationNeuralNetwork()
+nn.create_and_add_layer(2, 10, Sigmoid(), False)
+nn.create_and_add_layer(10, 15, Sigmoid(), False)
+nn.create_and_add_layer(15, 2, Sigmoid(), True)
 
-# train_data = create_train_data(ProblemType.Classification, "./data/classification/train/data.simple.train.1000.csv", ["x", "y"], ["cls"])
-# test_data = create_test_data(ProblemType.Classification, "./data/classification/test/data.simple.test.1000.csv", ["x", "y"], ["cls"])
-# nn.train(train_data, 10, 100, 0.5, 0, test_data)
+train_data = create_train_data(ProblemType.Classification, "./data/classification/train/data.simple.train.1000.csv", ["x", "y"], ["cls"])
+test_data = create_test_data(ProblemType.Classification, "./data/classification/test/data.simple.test.1000.csv", ["x", "y"], ["cls"])
+nn.train(train_data, 10, 100, 0.5, 0, test_data)
 
 # Regression
 print("Regression")
