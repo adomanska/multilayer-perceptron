35fe45d1fbc17c28c9cd800899cf33765e8b8c0e
Michal Dzidziewicz <dzidziewicz_michal@interia.pl>
2019-10-27 22:17:27 +0100

    added files and directories creation

--- src/report/report.py
+++ src/report/report.py
@@ -9,24 +9,53 @@ from cost_functions.quadratic_cost import QuadraticCost
 from data_transformations import create_test_data, create_train_data, ProblemType
 
 import numpy as np
+import os
 
 class Report:
     @staticmethod
-    def test():
-        batch_sizes = np.arange(1, 100, 10)
-        epoch_counts = np.arange(100, 1500, 100)
-        etas = np.arange(0.1, 1, 0.1)
-        momenta = np.arange(0, 1, 0.1)
-
+    def run_tests():
         cls_networks = Report.prepare_classification_networks()
         reg_networks = Report.prepare_regression_networks()
 
         train_data = create_train_data(ProblemType.Classification, "./data/classification/train/data.simple.train.1000.csv", ["x", "y"], ["cls"])
-        test_data = create_test_data(ProblemType.Classification, "./data/classification/test/data.simple.test.1000.csv", ["x", "y"], ["cls"])
+        test_data = create_train_data(ProblemType.Classification, "./data/classification/test/data.simple.test.1000.csv", ["x", "y"], ["cls"])
 
-        f = open('results.txt', 'w')
+        Report._test_networks(cls_networks, '/report/classification/classification.txt', train_data, test_data)
 
-        for nn in cls_networks:
+    @staticmethod
+    def test_classification():
+        output_directory = './report/classification/'
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        train_dir = './data/classification/train/'
+        test_dir = './data/classification/test/'
+        (_, _, train_filenames) = os.walk(train_dir).__next__()
+        (_, _, test_filenames) = os.walk(test_dir).__next__()
+
+        print(train_filenames)
+
+        cls_networks = Report.prepare_classification_networks()
+
+        for train_file, test_file in zip(train_filenames, test_filenames):
+            train_data = create_train_data(ProblemType.Classification, train_dir + train_file, ["x", "y"], ["cls"])
+            test_data = create_train_data(ProblemType.Classification, test_dir + test_file, ["x", "y"], ["cls"])
+
+            Report._test_networks(cls_networks, f'{output_directory}{train_file}/', train_data, test_data)
+
+    @staticmethod
+    def _test_networks(networks, output_dir, train_data, test_data):
+        batch_sizes = np.arange(10, 30, 10)
+        epoch_counts = np.arange(100, 300, 100)
+        etas = np.arange(0.1, 0.3, 0.1)
+        momenta = np.arange(0, 0.3, 0.1)
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        for index, nn in enumerate(networks):
+            f = open(f'{output_dir}/{index}.txt', 'w')
+
             for epoch_count in epoch_counts:
                 for eta in etas:
                     for momentum in momenta:
@@ -34,37 +63,60 @@ class Report:
                             nn.train(train_data, batch_size, epoch_count, eta, momentum, test_data)
                             f.write('START TEST \n')
                             f.write(f'Epochs: {epoch_count}, eta: {eta}, momentum: {momentum}, batch size: {batch_size}\n')
-                            # zaloguj nazwę zbioru i parametry sieci
+                            f.write(f'{type(nn).__name__}\n')
+                            f.write('Layers:\n')
+                            for layer in nn.layers:
+                                f.write(f'neurons: {layer.neuron_count}, activation: {type(layer.activation_function).__name__}\n')
+                            
+                            f.write(f'cost function: {type(nn.layers[-1].cost_function).__name__}\n')
                             f.write(f'Last accuracy: {nn.accuracies[-1]}\n')
                             f.write(f'Best accuracy: {np.max(nn.accuracies)}\n')
                             f.write('END TEST\n')
                             f.write('\n==================================================\n\n')
-        
-        f.close()
-                
+            f.close()
 
     @staticmethod
     def prepare_classification_networks():
+        networks = []
+        # no hidden layers
         cls1 = ClassificationNeuralNetwork()
-        cls1.create_and_add_layer(2, 10, Sigmoid(), False)
-        cls1.create_and_add_layer(10, 15, Sigmoid(), False)
-        cls1.create_and_add_layer(15, 2, Sigmoid(), True, QuadraticCost())
+        cls1.create_and_add_output_layer(2, 2, Sigmoid(), QuadraticCost())
+        networks.append(cls1)
 
-        cls2 = ClassificationNeuralNetwork()
-        cls2.create_and_add_layer(2, 10, Sigmoid(), False)
-        cls2.create_and_add_layer(10, 2, Sigmoid(), True, QuadraticCost())
+        # 1 hidden layer
+        cls1 = ClassificationNeuralNetwork()
+        cls1.create_and_add_hidden_layer(2, 1, Sigmoid())
+        cls1.create_and_add_output_layer(1, 2, Sigmoid(), QuadraticCost())
+        networks.append(cls1)
 
-        return [cls1, cls2]
+        cls1 = ClassificationNeuralNetwork()
+        cls1.create_and_add_hidden_layer(2, 5, Sigmoid())
+        cls1.create_and_add_output_layer(5, 2, Sigmoid(), QuadraticCost())
+        networks.append(cls1)
+
+        cls1 = ClassificationNeuralNetwork()
+        cls1.create_and_add_hidden_layer(2, 10, Sigmoid())
+        cls1.create_and_add_output_layer(10, 2, Sigmoid(), QuadraticCost())
+        networks.append(cls1)
+
+        # 2 hidden
+        cls1 = ClassificationNeuralNetwork()
+        cls1.create_and_add_hidden_layer(2, 1, Sigmoid())
+        cls1.create_and_add_hidden_layer(1, 5, Sigmoid())
+        cls1.create_and_add_output_layer(5, 2, Sigmoid(), QuadraticCost())
+        networks.append(cls1)
+
+        return networks
 
     @staticmethod
     def prepare_regression_networks():
         reg1 = RegressionNeuralNetwork()
-        reg1.create_and_add_layer(1, 9, Sigmoid(), False)
-        reg1.create_and_add_layer(9, 1, Linear(), True)
+        reg1.create_and_add_hidden_layer(1, 9, Sigmoid())
+        reg1.create_and_add_output_layer(9, 1, Linear(), QuadraticCost())
         
         reg2 = RegressionNeuralNetwork()
-        reg2.create_and_add_layer(1, 9, Sigmoid(), False)
-        reg2.create_and_add_layer(9, 15, Sigmoid(), False)
-        reg2.create_and_add_layer(15, 1, Linear(), True)
+        reg2.create_and_add_hidden_layer(1, 9, Sigmoid())
+        reg2.create_and_add_hidden_layer(9, 15, Sigmoid())
+        reg2.create_and_add_output_layer(15, 1, Linear(), QuadraticCost())
 
         return [reg1, reg2]
