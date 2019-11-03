7082d45e5cba8b9281b2a9a2f82a0413febb7e16
Andżelika Domańska <andzelika.domanska@prudential.pl>
2019-11-03 20:21:08 +0100

    Add softmax function

---
+++ src/activation_functions/softmax.py
@@ -0,0 +1,11 @@
+from .activation_function import ActivationFunction
+import numpy as np
+
+class Softmax(ActivationFunction):
+    def calculate(self, x):
+        x_stable = x - np.max(x, axis=0)
+        return np.exp(x_stable) / np.sum(np.exp(x_stable), axis=0, keepdims=True)
+
+    def calculate_derivative(self, x):
+        values = self.calculate_value(x)
+        return values * (1 - values)
